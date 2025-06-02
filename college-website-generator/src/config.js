81f59d2178ab13ff10331e01e9b7eb9fd038ab64
ashishrathod9 <133087879+ashishrathod9@users.noreply.github.com>
2025-06-02 11:25:51 +0530

    Update config.js

--- college-website-generator/src/config.js
+++ college-website-generator/src/config.js
@@ -1,6 +1,10 @@
-export default const config = {
+export default {
   // API URL for production
   API_URL: 'https://websitecreator-12.onrender.com/api',
   // Development API URL
-  
+  DEV_API_URL: 'http://localhost:5000/api',
+  // Use production URL by default
+  get API_BASE_URL() {
+    return process.env.NODE_ENV === 'production' ? this.API_URL : this.DEV_API_URL;
+  }
 }; 
